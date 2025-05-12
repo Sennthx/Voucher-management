3b84391ea29af323a73d4ad8ae22d3497b7ad904
znxa1x <63876124+Sennthx@users.noreply.github.com>
2025-05-12 22:00:12 +0200

    Created validation request

---
+++ voucher.management/src/main/java/com/wecan/voucher/management/validation/ValidRedemptionRequest.java
@@ -0,0 +1,17 @@
+package com.wecan.voucher.management.validation;
+
+import com.wecan.voucher.management.validation.impl.RedemptionRequestValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = RedemptionRequestValidator.class)
+@Target({ ElementType.TYPE }) // Applies at the class (record/object) level
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidRedemptionRequest {
+    String message() default "Invalid voucher redemption request";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
