5338b33c0300e7d559b9b0a53c1f4f6d00a9963b
znxa1x <63876124+Sennthx@users.noreply.github.com>
2025-05-12 21:45:38 +0200

    Added aditional cases for exceptions.

--- voucher.management/src/main/java/com/wecan/voucher/management/exception/GlobalExceptionHandler.java
+++ voucher.management/src/main/java/com/wecan/voucher/management/exception/GlobalExceptionHandler.java
@@ -2,13 +2,17 @@ package com.wecan.voucher.management.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,14 +47,6 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
-        return new ResponseEntity<>(
-                Map.of("error", ex.getReason()),
-                ex.getStatusCode()
-        );
-    }
-
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Map<String, String>> handleInvalidDateFormat(HttpMessageNotReadableException ex) {
         Throwable cause = ex.getCause();
@@ -73,8 +69,41 @@ public class GlobalExceptionHandler {
         );
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDenied(AccessDeniedException ex) {
+        return new ResponseEntity<>(
+                Map.of("error", "Access Denied: " + ex.getMessage()),
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        return new ResponseEntity<>(
+                Map.of("error", "Endpoint not found. Check your route."),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        HttpStatusCode status = ex.getStatusCode(); // use the original status
+        String message = ex.getReason() != null ? ex.getReason() : "Unexpected error";
+
+        return new ResponseEntity<>(
+                Map.of("error", message),
+                status
+        );
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoResourceFoundException(NoResourceFoundException ex) {
+        return new ResponseEntity<>(Map.of("error", "404 Not found"), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleAll(Exception ex) {
         return new ResponseEntity<>(Map.of("error", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
