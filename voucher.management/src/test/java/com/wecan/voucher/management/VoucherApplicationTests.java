8c1d2e1b3187e7844adab989ce7f190ac9b96163
znxa1x <63876124+Sennthx@users.noreply.github.com>
2025-05-13 15:59:14 +0200

    Created basic spring boot test

--- voucher.management/src/test/java/com/wecan/voucher/management/VoucherApplicationTests.java
+++ voucher.management/src/test/java/com/wecan/voucher/management/VoucherApplicationTests.java
@@ -1,13 +1,29 @@
 package com.wecan.voucher.management;
 
+import com.wecan.voucher.management.voucherSystem.repository.VoucherRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class VoucherApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private VoucherRepository voucherRepository;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context, "Application context should be loaded");
+		assertNotNull(voucherRepository, "VoucherRepository should be available in the context");
 	}
 
+	@Test
+	void databaseConnectivityTest() {
+		assertTrue(voucherRepository.count() >= 0, "Repository should interact with the database successfully");
+	}
 }
